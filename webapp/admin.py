a2f7a5ccb6b7f1b198cb78d3d731909061dfb455
Assym <asimo_555@mail.ru>
2022-07-02 15:14:41 +0600

    Добавлена модель, миграцию, зарегистрировали модель в админке

--- webapp/admin.py
+++ webapp/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
 # Register your models here.
+from webapp.models import Article
+
+
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ['id', 'authorName', 'authorEmail', 'created_at']
+    list_display_links = ['authorName']
+    list_filter = ['authorName', 'authorEmail']
+    search_fields = ['authorName', 'authorEmail', 'content']
+    fields = ['authorName', 'authorEmail', 'content', 'created_at', 'updated_at']
+    readonly_fields = ['created_at', 'updated_at']
+
+
+admin.site.register(Article, ArticleAdmin)
--- webapp/models.py
+++ webapp/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+STATUS_CHOICES = [('active', 'Активно'), ('blocked', 'Заблокировано')]
+
+class Article(models.Model):
+    authorName = models.CharField(max_length=50, null=False, blank=False, verbose_name="имя автора")
+    authorEmail = models.CharField(max_length=50, verbose_name="email автора")
+    content = models.TextField(max_length=3000, verbose_name="Текст")
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата создания")
+    updated_at = models.DateTimeField(auto_now=True, verbose_name="Дата изменения")
+    status = models.CharField(max_length=50, null=False, verbose_name="Статус", default=STATUS_CHOICES[0][0], choices=STATUS_CHOICES)
+
+    def __str__(self):
+        return f"{self.id}. {self.authorName}: {self.authorEmail}"
+
+    class Meta:
+        db_table = "articles"
+        verbose_name = "Статья"
+        verbose_name_plural = "Статьи"
